01f7c488d2ed5e0052379923bcce748b1f755154
kimminkyu <kmk50009@naver.com>
2021-09-03 07:55:28 +0900

    Studying Initial Commit

---
+++ app/src/main/java/com/elliot/studying/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.elliot.studying
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.elliot.studying.ui.theme.StudyingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StudyingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    StudyingTheme {
+        Greeting("Android")
+    }
+}
