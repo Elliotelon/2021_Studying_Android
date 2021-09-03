82054e27e58f37d2d7a87d0298ba25067bfd8044
kimminkyu <kmk50009@naver.com>
2021-09-04 00:32:50 +0900

    Studying Step01Row

--- app/src/main/java/com/elliot/studying/MainActivity.kt
+++ app/src/main/java/com/elliot/studying/MainActivity.kt
@@ -3,11 +3,21 @@ package com.elliot.studying
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.elliot.studying.ui.theme.StudyingTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +27,58 @@ class MainActivity : ComponentActivity() {
             StudyingTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    HelloScreen()
                 }
             }
         }
     }
 }
 
+class HelloViewModel : ViewModel() {
+
+    // LiveData holds state which is observed by the UI
+    // (state flows down from ViewModel)
+    private val _name = MutableLiveData("")
+    val name: LiveData<String> = _name
+
+    // onNameChange is an event we're defining that the UI can invoke
+    // (events flow up from UI)
+    fun onNameChange(newName: String) {
+        _name.value = newName
+    }
+}
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HelloScreen(helloViewModel: HelloViewModel = viewModel()) {
+    // by default, viewModel() follows the Lifecycle as the Activity or Fragment
+    // that calls HelloScreen(). This lifecycle can be modified by callers of HelloScreen.
+
+    // name is the current value of [helloViewModel.name]
+    // with an initial value of ""
+    val name: String by helloViewModel.name.observeAsState("")
+    HelloContent(name = name, onNameChange = { helloViewModel.onNameChange(it) })
+}
+
+@Composable
+fun HelloContent(name: String, onNameChange: (String) -> Unit) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = "Hello, $name",
+            modifier = Modifier.padding(bottom = 8.dp),
+            style = MaterialTheme.typography.h5
+        )
+        OutlinedTextField(
+            value = name,
+            onValueChange = onNameChange,
+            label = { Text("Name") }
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     StudyingTheme {
-        Greeting("Android")
+        HelloScreen()
     }
 }
