2316e05c7b78a8fb481c9543b2f1f9d6e74d65f1
kimminkyu <kmk50009@naver.com>
2021-09-06 16:52:31 +0900

    Studying Step02Column

--- app/src/main/java/com/elliot/studying/MainActivity.kt
+++ app/src/main/java/com/elliot/studying/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.elliot.studying
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -27,7 +28,10 @@ class MainActivity : ComponentActivity() {
             StudyingTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
+                    Log.d("a", "a1")
                     HelloScreen()
+                    Log.d("a", "a2")
+
                 }
             }
         }
@@ -35,16 +39,18 @@ class MainActivity : ComponentActivity() {
 }
 
 class HelloViewModel : ViewModel() {
-
     // LiveData holds state which is observed by the UI
     // (state flows down from ViewModel)
     private val _name = MutableLiveData("")
+
     val name: LiveData<String> = _name
 
     // onNameChange is an event we're defining that the UI can invoke
     // (events flow up from UI)
     fun onNameChange(newName: String) {
         _name.value = newName
+        Log.d("a", "a3")
+
     }
 }
 
@@ -55,13 +61,23 @@ fun HelloScreen(helloViewModel: HelloViewModel = viewModel()) {
 
     // name is the current value of [helloViewModel.name]
     // with an initial value of ""
+    Log.d("a", "a4")
+
     val name: String by helloViewModel.name.observeAsState("")
+    Log.d("a", "a5")
+
     HelloContent(name = name, onNameChange = { helloViewModel.onNameChange(it) })
+    Log.d("a", "a6")
+
 }
 
 @Composable
 fun HelloContent(name: String, onNameChange: (String) -> Unit) {
+    Log.d("a", "a7")
+
     Column(modifier = Modifier.padding(16.dp)) {
+        Log.d("a", "a8")
+
         Text(
             text = "Hello, $name",
             modifier = Modifier.padding(bottom = 8.dp),
@@ -72,6 +88,8 @@ fun HelloContent(name: String, onNameChange: (String) -> Unit) {
             onValueChange = onNameChange,
             label = { Text("Name") }
         )
+        Log.d("a", "a9")
+
     }
 }
 
--- step02column/src/main/java/com/elliot/step02column/MainActivity.kt
+++ step02column/src/main/java/com/elliot/step02column/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.elliot.step01row
+package com.elliot.step02column
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -14,7 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.elliot.step01row.ui.theme.StudyingTheme
+import com.elliot.step02column.ui.theme.StudyingTheme
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
@@ -24,41 +24,21 @@ class MainActivity : ComponentActivity() {
             StudyingTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                   Container()
+                    VerticalContainer()
                 }
             }
         }
     }
 }
-// arrangement 요소를 어떤식 배열할지
-// arrangement 는 Row, Column 같은 요소들이 들어가는
-// 컨테이너성격의 콤포저블에서 요소들의 아이템을 정렬할 때 사용 됩니다.
-// 웹 개발 css 에서 flex 와 비슷하다고 보시면 됩니다.
-
-// horizontal Arrangement 이니까 Start, End, Center 만 존재
-//Arrangement.SpaceBetween : 공간 모두 차지
-//Arrangement.Start : 왼쪽으로
-//Arrangement.End : 오른쪽으로
-//Arrangement.SpaceAround : 빈 공간을 남겨두기
-//Arrangement.Center : 요소들에 넣기
-//Arrangement.SpaceBetween : 사이에 공간을 밀어넣기
-//Arrangement.SpaceEvenly : 요소들 사이에 공간을 똑같이 하기
-
-// alignment 는 말그대로 해당 컨테이너 안에 들어있는 요소들의 위치를 어떠한 방향으로 정렬할지를 정합니다.
-// linearLayout 에서 gravity 와 동일 하다고 보시면 됩니다.
-// Alignment.Bottom : 컨테이너의 아래에 두기
-// Alignment.Top : 컨테이너의 위에 두기
-// 현재는 Row 콤포저블 안에서 align이 들어가기 때문에 Center Vertically
-// Alignment.CenterVertically : 컨테이너의 수직방향으로 중앙에 두기
 
 @Composable
-fun Container(){
-    Row(
+fun VerticalContainer(){
+    Column(
         modifier = Modifier
             .background(Color.White)
             .fillMaxSize(),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.Bottom
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
     ) {
         DummyBox()
         DummyBox()
@@ -82,6 +62,6 @@ fun DummyBox(modifier : Modifier = Modifier){
 @Composable
 fun DefaultPreview() {
     StudyingTheme {
-        Container()
+        VerticalContainer()
     }
 }
